5be288e4aa5b77593d2aa93a8c72e05e20a3c364
Biruk Dereje <biruk.scorpio.dereje@gmail.com>
2025-01-15 20:52:54 +0300

    Finished Javascript

---
+++ Assessment-3_Biruk-Dereje-Getachew_UGR-6190-15_Section-1/src/js/calculator.js
@@ -0,0 +1,96 @@
+document.addEventListener("DOMContentLoaded", function () {
+   const display = document.getElementById("display");
+   const buttons = document.querySelectorAll(".btn");
+   let currentInput = "";
+   let operator = "";
+   let firstOperand = "";
+
+   buttons.forEach((button) => {
+      button.addEventListener("click", function () {
+         const value = this.getAttribute("data-value");
+         console.log(value);
+
+         if (value === "C") {
+            currentInput = "";
+            display.value = "";
+         } else if (value === "AC") {
+            currentInput = "";
+            operator = "";
+            firstOperand = "";
+            display.value = "";
+         } else if (value === "+/-") {
+            if (currentInput) {
+               currentInput = (parseFloat(currentInput) * -1).toString();
+               display.value = currentInput;
+            }
+         } else if (value === "=") {
+            if (firstOperand && operator && currentInput) {
+               const secondOperand = parseFloat(currentInput);
+               if (secondOperand === 0 && operator === "/") {
+                  display.value = "Error";
+               } else {
+                  let result;
+                  switch (operator) {
+                     case "+":
+                        result = firstOperand + secondOperand;
+                        break;
+                     case "-":
+                        result = firstOperand - secondOperand;
+                        break;
+                     case "*":
+                        result = firstOperand * secondOperand;
+                        break;
+                     case "/":
+                        result = firstOperand / secondOperand;
+                        break;
+                  }
+                  display.value = result;
+               }
+               currentInput = "";
+               firstOperand = "";
+               operator = "";
+            }
+         } else if (["+", "-", "*", "/"].includes(value)) {
+            if (currentInput && !firstOperand) {
+               firstOperand = parseFloat(currentInput);
+               operator = value;
+               currentInput = "";
+            } else if (currentInput && firstOperand) {
+               const secondOperand = parseFloat(currentInput);
+               let result;
+               switch (operator) {
+                  case "+":
+                     result = firstOperand + secondOperand;
+                     break;
+                  case "-":
+                     result = firstOperand - secondOperand;
+                     break;
+                  case "*":
+                     result = firstOperand * secondOperand;
+                     break;
+                  case "/":
+                     result = firstOperand / secondOperand;
+                     break;
+               }
+               firstOperand = result;
+               operator = value;
+               currentInput = "";
+               display.value = result;
+            }
+         } else {
+            if (currentInput === "0" && value === "0" && !operator) {
+               currentInput = "0";
+               display.value = currentInput;
+            } else if (value === "0" && operator === "/") {
+               display.value = "Error";
+               currentInput = "";
+               firstOperand = "";
+               operator = "";
+            } else {
+               currentInput += value;
+               display.value = currentInput;
+            }
+         }
+      });
+   });
+});
